c162acd9aeb9484ab506439e5b24218f8d7da480
masrudmubarok <masrud.mubarok21@gmail.com>
2022-08-07 16:35:32 +0700

    Update Contact List Componnent

--- frontend/src/component/ContactList.js
+++ frontend/src/component/ContactList.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
 
 const ContactList = () => {
+    const [contacts, setContact] = useState([]);
+
+    useEffect(() => {
+        getContacts();
+    },[]);
+
+    const getContacts = async () => {
+        const response = await axios.get('http://localhost:5000/contacts');
+        setContact(response.data);
+    }
+
   return (
     <div className="columns mt-5 is-centered">
         <div className="column is-half">
-            <h1>Lorem ipsum dolor sit amet.</h1>
+            <table className="table is-striped is-fullwidth">
+                <thead>
+                    <tr>
+                        <th className="has-text-centered">No</th>
+                        <th className="has-text-centered">Name</th>
+                        <th className="has-text-centered">Phone</th>
+                        <th className="has-text-centered">Email</th>
+                        <th className="has-text-centered">Gender</th>
+                        <th className="has-text-centered">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {contacts.map((contact, index) => (
+                        <tr key={contact.id}>
+                            <td>{index + 1}</td>
+                            <td>{contact.name}</td>
+                            <td>{contact.phone}</td>
+                            <td>{contact.email}</td>
+                            <td>{contact.gender}</td>
+                            <td>
+                                <button className="button is-small is-info">Edit</button>
+                                <button className="button is-small is-danger">Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     </div>
   )
